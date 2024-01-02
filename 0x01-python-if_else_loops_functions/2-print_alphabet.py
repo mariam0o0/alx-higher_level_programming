ce1973dd8b2e6a6d4cb109cc693c392cf3003e12
Mariam <mariam.yasser772000@gmail.com>
2024-01-02 13:36:40 -0800

    0x01-python

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for character in range(ord('a'), ord('z') + 1):
-    print('{:c}'.format(character), end='')
-
+for char in range(ord('a'), ord('z') + 1):
+    print('{:c}'.format(char), end='')
