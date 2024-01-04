3a10caa47c27d07678e130141fa5a38c3446634d
Mariam <mariam.yasser772000@gmail.com>
2024-01-04 10:17:53 -0800

    0x02-python

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-import sys
-no_of_args = len(sys.argv) - 1
-arguments = sys.argv[1:]
-if no_of_args == 0:
-    print("0 arguments.")
-elif no_of_args == 1:
-    print("1 argument.")
-else:
-    print("{} arguments:".format(no_of_args))
-for i in range(no_of_args):
-    print ("{}: {}".format(i + 1 , sys.argv[i + 1]))
+if __name__ == "__main__":
+    import sys
+    no_of_args = len(sys.argv) - 1
+    arguments = sys.argv[1:]
+    if no_of_args == 0:
+        print("0 arguments.")
+    elif no_of_args == 1:
+        print("1 argument.")
+    else:
+        print("{} arguments:".format(no_of_args))
+    for i in range(no_of_args):
+        print ("{}: {}".format(i + 1 , sys.argv[i + 1]))
