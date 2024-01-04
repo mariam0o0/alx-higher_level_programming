1bc796217896edd92ed88aefbf2889acf09d6646
Mariam <mariam.yasser772000@gmail.com>
2024-01-04 10:30:00 -0800

    0x02-python

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,9 +3,9 @@ if __name__ == "__main__":
     import sys
     no_of_args = len(sys.argv) - 1
     if no_of_args == 0:
-        print("{}".format("0 arguments."))
+        print("0 arguments.")
     elif no_of_args == 1:
-        print("1 argument.")
+        print("1 argument:")
     else:
         print("{} arguments:".format(no_of_args))
     for i in range(no_of_args):
