174c35c1336ae806690ae8838c651aa2c2e36d2f
Mariam <mariam.yasser772000@gmail.com>
2024-01-07 12:41:54 -0800

    0x03

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,7 @@
+def multiple_returns(sentence):
+    if not sentence:
+        first_ch = None
+    else:
+        first_ch = sentence[0]
+    my_tuple = len(sentence), first_ch
+    return (my_tuple)
