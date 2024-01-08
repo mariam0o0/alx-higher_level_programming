2b69ebc9906b0a08f3a1f3e20a8d0a098d9f173f
Mariam <mariam.yasser772000@gmail.com>
2024-01-08 08:41:24 -0800

    0x03

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    my_list = list(my_string)
-    if 'c' in my_list:
-        my_list.remove('c')
-    if 'C' in my_list:
-        my_list.remove('C')
-    return ''.join(my_list)
+    str = ''
+    for i in my_string:
+        if i not in ('c', 'C'):
+            str += i
+    return str
