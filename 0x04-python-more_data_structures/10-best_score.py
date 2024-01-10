ec99b592ee6da7a088d1bca1e62735f8108b6621
Mariam <mariam.yasser772000@gmail.com>
2024-01-10 12:28:35 -0800

    0x04

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary:
-        return max(a_dictionary)
-    else:
+    if not a_dictionary:
         return None
+    else:
+        return max(a_dictionary)
