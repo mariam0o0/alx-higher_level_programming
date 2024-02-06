221fda8c9201edf7714702e212ef63dfd6683252
Mariam <mariam.yasser772000@gmail.com>
2024-02-06 11:01:23 -0800

    0x0B

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-"""
-Module that adds args to JSON file
-"""
+'''
+Module to add all arguments to a Python list save them to a file
+'''
 
 import sys
 from 5_save_to_json_file import save_to_json_file
 from 6_load_from_json_file import load_from_json_file
+import os
 
+arg_list = sys.argv[1:]
 
-def add_to_list_and_save(arguments):
-    """adds all arguments to a Python list and save them to a file"""
-    try:
-        existing_list = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        existing_list = []
-    existing_list.extend(arguments)
-    save_to_json_file(existing_list, "add_item.json")
+lisst = []
+if os.path.exists('add_item.json'):
+    lisst = load_from_json_file('add_item.json')
+
+save_to_json_file(lisst + arg_list, "add_item.json")
