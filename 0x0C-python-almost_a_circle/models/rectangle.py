dbeaa4bd5c876c76e71009c4d7ff7e080cbd729b
Mariam <mariam.yasser772000@gmail.com>
2024-02-12 09:34:18 -0800

    0x0C

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -24,3 +24,47 @@ class Rectangle(Base):
             raise ValueError("{} must be > 0".format(name))
         if not condition and value < 0:
             raise ValueError("{} must be >= 0".format(name))
+
+    @property
+    def width(self):
+        """The width getter"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """The width setter"""
+        self.validation('width', value)
+        self.__width = value
+
+    @property
+    def height(self):
+        """The height getter"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """The height setter"""
+        self.validation('height', value)
+        self.__height = value
+
+    @property
+    def x(self):
+        """The x getter"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """The x setter"""
+        self.validation('x', value, False)
+        self.__x = value
+
+    @property
+    def y(self):
+        """The y getter"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """The y setter"""
+        self.validation('y', value, False)
+        self.__y = value
