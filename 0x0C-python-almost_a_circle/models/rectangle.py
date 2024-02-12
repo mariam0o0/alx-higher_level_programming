6146b86e421d9cb4864a3e47d6531991db7f9320
Mariam <mariam.yasser772000@gmail.com>
2024-02-12 09:29:14 -0800

    0x0C

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -9,95 +9,9 @@ class Rectangle(Base):
     """The Rectangle Class"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """The class init"""
+        """ The class init"""
         super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-
-    def validation(self, name, value, condition=True):
-        """Validate the value and type of attributes"""
-        if type(value) != int:
-            raise TypeError(f"{name} must be an integer")
-        if condition and value <= 0:
-            raise ValueError(f"{name} must be > 0")
-        if not condition and value < 0:
-            raise ValueError(f"{name} must be >= 0")
-
-    @property
-    def width(self):
-        """The width getter"""
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """The width setter"""
-        self.validation('width', value)
-        self.__width = value
-
-    @property
-    def height(self):
-        """The height getter"""
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """The height setter"""
-        self.validation('height', value)
-        self.__height = value
-
-    @property
-    def x(self):
-        """The x getter"""
-        return self.__x
-
-    @x.setter
-    def x(self, value):
-        """The x setter"""
-        self.validation('x', value, False)
-        self.__x = value
-
-    @property
-    def y(self):
-        """The y getter"""
-        return self.__y
-
-    @y.setter
-    def y(self, value):
-        """The y setter"""
-        self.validation('y', value, False)
-        self.__y = value
-
-    def area(self):
-        """Return the area of Rectangle instance"""
-        return self.width * self.height
-
-    def __str__(self):
-        """String representation"""
-        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
-
-    def display(self):
-        """Print the Rectangle with the character #"""
-        for _ in range(self.y):
-            print()
-        for _ in range(self.height):
-            print(' ' * self.x + '#' * self.width, end='')
-        print()
-
-    def update(self, *args, **kwargs):
-        """Assign an argument to each attribute"""
-        full = (self.id, self.width, self.height, self.x, self.y)
-        if args != ():
-            self.id, self.width, self.height, self.x, self.y = \
-                args + full[len(args):]
-        elif kwargs:
-            for name, value in kwargs.items():
-                setattr(self, name, value)
-
-    def to_dictionary(self):
-        """Dictionary representation of a Rectangle"""
-        return {
-            'x': self.x, 'width': self.width, 'id': self.id,
-            'height': self.height, 'y': self.y}
-
