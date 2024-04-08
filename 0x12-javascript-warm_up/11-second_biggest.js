a3089b1e93c27cc89c198e3c0a496c83f648a9a8
Mariam <mariam.yasser772000@gmail.com>
2024-04-08 15:08:15 -0700

    0x012

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 // script that searches the second biggest integer in a list of arguments
 
-if (process.argv.length <= 3) {
+const argl = process.argv;
+if (argl.length <= 3) {
   console.log(0);
 } else {
-  const arglist = process.argv.sort();
-  console.log(arglist.reverse()[1]);
+  console.log(argl.sort((x, y) => y - x).slice(3)[0]);
 }
