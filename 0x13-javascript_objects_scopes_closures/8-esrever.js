b69e73e6f1a45abf0a49ba168b323be6b5e6998f
Mariam <mariam.yasser772000@gmail.com>
2024-04-09 13:14:20 -0700

    0x13

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
   if (list === undefined) return [];
-  let rev = [];
-  for (let len = list.length - 1; len >= 0; len--) {
+  const rev = [];
+  for (let len = list.len - 1; len >= 0; len--) {
     rev.push(list[len]);
   }
   return rev;
