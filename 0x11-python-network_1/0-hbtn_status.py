f0377480c2ab815cd8a27961749f751951c2694d
Mariam <mariam.yasser772000@gmail.com>
2024-04-28 08:01:56 -0700

    0x11

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,7 +4,8 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen(https://alx-intranet.hbtn.io/status) as response:
+    link = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(link) as response:
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
